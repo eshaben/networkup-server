0c4a200b48cfb48c54a0dac263d7c0afa4eb7649
Erin Shaben <eshaben@icloud.com>
2017-10-24 11:36:30 -0600

    set up objection.js and cors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const knex = require('./db/knex')
+const cors = require('cors')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', index);
 app.use('/users', users);
