1eec44089c3b0679c77dfa6c4bf6e2a2481a1f91
Erin Shaben <eshaben@icloud.com>
2017-10-31 18:26:50 -0600

    added real seed data for badges and challenges

--- seeds/05_challenges.js
+++ seeds/05_challenges.js
@@ -1,16 +1,106 @@
 
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM "challenge"; ALTER SEQUENCE challenge_id_seq RESTART WITH 3;')
+  return knex.raw('DELETE FROM "challenge"; ALTER SEQUENCE challenge_id_seq RESTART WITH 21;')
     .then(function () {
       var challenges = [{
         id: 1,
-        description: 'Connect with one person you met at the last event on LinkedIn',
-        points: 1000,
+        description: 'Attend your first event',
+        points: 100,
       },
       {
         id: 2,
-        description: 'Email one person you met at the last event and ask them to get coffee',
+        description: 'Attend five events',
+        points: 250,
+      },
+      {
+        id: 3,
+        description: 'Attend ten events',
+        points: 500,
+      },
+      {
+        id: 4,
+        description: 'Attend 25 events',
         points: 1000,
+      },
+      {
+        id: 5,
+        description: 'Attend 50 events',
+        points: 1500,
+      },
+      {
+        id: 6,
+        description: 'Attend 100 events',
+        points: 3000,
+      },
+      {
+        id: 7,
+        description: 'Attend 250 events',
+        points: 4500,
+      },
+      {
+        id: 8,
+        description: 'Attend 500 events',
+        points: 7000,
+      },
+      {
+        id: 9,
+        description: 'Help one person',
+        points: 200,
+      },
+      {
+        id: 10,
+        description: 'Help five people',
+        points: 600,
+      },
+      {
+        id: 11,
+        description: 'Help ten people',
+        points: 1500,
+      },
+      {
+        id: 12,
+        description: 'Help 25 people',
+        points: 4000,
+      },
+      {
+        id: 13,
+        description: 'Introduce two of your connections',
+        points: 500,
+      },
+      {
+        id: 14,
+        description: 'Have conversations with 5 different people',
+        points: 125,
+      },
+      {
+        id: 15,
+        description: 'Have conversations with ten different people',
+        points: 250,
+      },
+      {
+        id: 16,
+        description: 'Have conversations with 25 different people',
+        points: 500,
+      },
+      {
+        id: 17,
+        description: 'Have conversations with 50 different people',
+        points: 1000,
+      },
+      {
+        id: 18,
+        description: 'Have conversations with 100 different people',
+        points: 2000,
+      },
+      {
+        id: 19,
+        description: 'Have conversations with 250 different people',
+        points: 4000,
+      },
+      {
+        id: 20,
+        description: 'Have conversations with 500 different people',
+        points: 7000,
       }];
       return knex('challenge').insert(challenges);
     });
--- seeds/07_badges.js
+++ seeds/07_badges.js
@@ -1,42 +1,42 @@
 
 exports.seed = function(knex, Promise) {
-  return knex.raw('DELETE FROM "badge"; ALTER SEQUENCE badge_id_seq RESTART WITH 2;')
+  return knex.raw('DELETE FROM "badge"; ALTER SEQUENCE badge_id_seq RESTART WITH 7;')
     .then(function () {
       var badges = [{
         id: 1,
-        points_needed: 0,
-        image: 'coming soon',
-        title: 'Networking Noob',
+        points_needed: 100,
+        image: 'https://drive.google.com/open?id=0B1D72U3Cb3MJOGFuNU5CUEtHcHM',
+        title: 'Newbie',
       },
       {
         id: 2,
-        points_needed: 2000,
-        image: 'coming soon',
-        title: 'Networking Skilled',
+        points_needed: 1000,
+        image: 'https://drive.google.com/open?id=0B1D72U3Cb3MJQ0VsS1Voa1hRd1E',
+        title: 'Amateur',
       },
       {
         id: 3,
-        points_needed: 0,
-        image: 'coming soon',
-        title: 'Networking Intermidiate',
+        points_needed: 5000,
+        image: 'https://drive.google.com/open?id=0B1D72U3Cb3MJU3hPSXpLQXlvVms',
+        title: 'Skilled',
       },
       {
         id: 4,
-        points_needed: 0,
+        points_needed: 10000,
         image: 'coming soon',
-        title: 'Networking Proficient',
+        title: 'https://drive.google.com/file/d/0B1D72U3Cb3MJaFA4QUNmZjM3R0E/view',
       },
       {
         id: 5,
-        points_needed: 0,
-        image: 'coming soon',
-        title: 'Networking Advanced',
+        points_needed: 25000,
+        image: 'https://drive.google.com/open?id=0B1D72U3Cb3MJX0hPaHVjU19MbGs',
+        title: 'Expert',
       },
       {
         id: 6,
-        points_needed: 0,
-        image: 'coming soon',
-        title: 'Networking Expert',
+        points_needed: 41000,
+        image: 'https://drive.google.com/open?id=0B1D72U3Cb3MJVTQwZlk5c05HbWc',
+        title: 'Supreme Master',
       }];
       return knex('badge').insert(badges);
     });
